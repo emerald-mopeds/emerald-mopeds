490a1925158b712b10d266970e3e3258440d6fc1
ammarm08 <ammar.mian08@gmail.com>
2015-11-11 15:57:48 -0800

    basic file structure for client

---
+++ client/scripts/app.js
@@ -0,0 +1,33 @@
+
+/*
+MODELS AND COLLECTIONS
+*/
+
+
+// Job Model
+
+
+// Job Collection
+
+
+// App Model
+
+
+
+/*
+VIEWS
+*/
+
+// Job View --> connected to Job Model
+
+
+
+// Jobs List View --> connected to Jobs Collection
+
+
+
+// Job Entry View (form) --> connected to Jobs Collection
+
+
+
+// App View
